4939b539162b19f922ce15bea73225fdbb47aba6
veyogndz <veyselgnduz@gmail.com>
2022-08-15 19:16:11 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Veysel Gündüz on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
